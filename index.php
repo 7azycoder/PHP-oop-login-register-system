69d3d58beae23318836f9dfeed238043d46af5d6
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-05-05 12:01:54 +0530

    Fixed Permissions issue

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
+<<<<<<< HEAD
+<?php
+require_once 'core/init.php';
+
+if(Session::exists('home')){
+	echo '<p>' . Session::flash('home') . '<p>';
+}
+
+$user = new User();
+if($user->isLoggedIn()){
+?>
+	<p>Hello <a href="profile.php?user=<?php echo escape($user->data()->username); ?>"><?php echo escape($user->data()->username); ?></a>!</p>
+
+	<ul>
+		<li><a href="logout.php">Log out</a></li>
+		<li><a href="update.php">Update Details</a></li>
+		<li><a href="changepassword.php">Change Password</a></li>
+
+	</ul>
+
+<?php
+	if($user->hasPermission('admin')){
+		echo '<p>You are an administrator</p>';
+	}
+	elseif ($user->hasPermission('member')) {
+		echo '<p>You are a member</p>';
+	} 
+	elseif($user->hasPermission('unregistered')) {
+		echo '<p>You need to activate your account first</p>';
+	}
+
+
+} else {
+	echo '<p>You need to <a href="login.php">login</a> or <a href="register.php">register</a></p>';
+=======
 <?php
 require_once 'core/init.php';
 
@@ -25,4 +60,5 @@ if($user->isLoggedIn()){
 
 } else {
 	echo '<p>You need to <a href="login.php">login</a> or <a href="register.php">register</a></p>';
+>>>>>>> e0390716fe6120a72d65e50437db239b838e75f1
 }
--- lovepreet.sql
+++ lovepreet.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Jul 24, 2015 at 09:53 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
+-- Host: localhost
+-- Generation Time: May 05, 2016 at 08:30 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `lovepreet`
@@ -26,12 +26,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `groups`
 --
 
-CREATE TABLE IF NOT EXISTS `groups` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `permissions` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  `permissions` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `groups`
@@ -39,7 +38,9 @@ CREATE TABLE IF NOT EXISTS `groups` (
 
 INSERT INTO `groups` (`id`, `name`, `permissions`) VALUES
 (1, 'Standard user', ''),
-(2, 'Administrator', '{\r\n"admin": 1,\r\n"moderator": 1\r\n}');
+(2, 'Administrator', '{\r\n"admin": 1,\r\n"moderator": 1\r\n}'),
+(3, 'Member', '{ "member": 1}'),
+(4, 'UnRegistered', '{"unregistered": 1}');
 
 -- --------------------------------------------------------
 
@@ -47,16 +48,15 @@ INSERT INTO `groups` (`id`, `name`, `permissions`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(20) NOT NULL,
   `password` varchar(64) NOT NULL,
   `salt` varchar(32) NOT NULL,
   `name` varchar(50) NOT NULL,
   `joined` datetime NOT NULL,
-  `group` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
+  `group` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -64,13 +64,53 @@ CREATE TABLE IF NOT EXISTS `users` (
 -- Table structure for table `users_session`
 --
 
-CREATE TABLE IF NOT EXISTS `users_session` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `users_session` (
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `hash` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+  `hash` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users_session`
+--
+ALTER TABLE `users_session`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+--
+-- AUTO_INCREMENT for table `users_session`
+--
+ALTER TABLE `users_session`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
